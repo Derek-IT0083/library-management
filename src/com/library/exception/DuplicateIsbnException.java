5bf8838492ad0cab3af663177b68f4ad5c44c10f
Derek-IT0083 <guanting219@gmail.com>
2026-08-23 11:52:43 +0800

    update 08/23

---
+++ src/com/library/exception/DuplicateIsbnException.java
@@ -0,0 +1,12 @@
+package com.library.exception;
+
+/**
+ * 新增藏書時 ISBN 已存在。
+ */
+
+public class DuplicateIsbnException extends LibraryException {
+
+    public DuplicateIsbnException(String isbn) {
+        super("ISBN 已存在：" + isbn);
+    }
+}
